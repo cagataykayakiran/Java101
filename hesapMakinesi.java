4d010e800c0dad6877cc9f0b4c96c9e2e9ae5c5a
Çağatay Kayakıran <73523962+cagataykayakiran@users.noreply.github.com>
2022-09-08 13:36:46 +0300

    Create hesapMakinesi.java

---
+++ hesapMakinesi.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        int select;
+        Scanner input = new Scanner(System.in);
+        System.out.print("sayi1 giriniz: ");
+        int sayi1 = input.nextInt();
+        System.out.print("sayi2 giriniz: ");
+        int sayi2 = input.nextInt();
+
+        System.out.println("Hangi işlemi istersiniz: \n:1-toplama\n:2-cikarma\n:3-carpma\n:4-bolme");
+        select = input.nextInt();
+
+        switch (select) {
+            case 1:
+                System.out.println("Sonucunuz: " + (sayi1 + sayi2));
+                break;
+            case 2:
+                System.out.println("Sonucunuz:" + (sayi1 - sayi2));
+                break;
+            case 3:
+                System.out.println("Sonucunuz:" + (sayi1 * sayi2));
+                break;
+            case 4:
+                System.out.println("Sonucunuz:" + (sayi1 / sayi2));
+                break;
+            default:
+                System.out.println("hatalı işlem...");
+                break;
+        }
+
+        }
+    }
+
